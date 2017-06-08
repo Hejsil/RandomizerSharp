125627b45c86d54f253c459ddf049b8dc8892a02
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2017-06-08 23:33:14 +0200

    Moved AbilityViewModel

--- RandomizerSharp.UI/ViewModels/AbilityViewModel.cs
+++ RandomizerSharp.UI/ViewModels/AbilityViewModel.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using RandomizerSharp.PokemonModel;
 
-namespace RandomizerSharp.UI.ViewModels.Commands
+namespace RandomizerSharp.UI.ViewModels
 {
     public class AbilityViewModel : BaseViewModel
     {
